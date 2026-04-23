6112c0c8e2ee4ddd7e4ef0abd73df1f05d136525
kayqueferreira185-tech <kayqueferreira185@gmail.com>
2026-04-23 11:15:10 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+const body = document.querySelector('body');
+const button = document.querySelector('button');
+
+button.addEventListener('click',() => {
+    body.classList.toggle('dark-mode');
+});
